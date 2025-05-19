7ad608fbedb11fa6181a6a47ee8bab753f1100a1
Jeff Romine <jeff@nightheron.dev>
2025-05-19 12:35:28 -0600

    Finished with slightly modified "One Click Order" version

--- temporal/src/workflows.ts
+++ temporal/src/workflows.ts
@@ -1,5 +1,5 @@
 import { proxyActivities, sleep } from '@temporalio/workflow';
-import type * as activities from './activities.ts';
+import type * as activities from './activities';
 
 const { purchase } = proxyActivities<typeof activities>({
   startToCloseTimeout: '1 minute',
